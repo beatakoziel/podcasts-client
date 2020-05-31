a43dd57cb8dc4821e8b25e0cf7d1cc610a695827
Beata <beatainez.k@gmail.com>
2020-05-31 18:53:42 +0200

    uploading podcast file

--- src/AdminPanel.vue
+++ src/AdminPanel.vue
@@ -1,19 +1,107 @@
 <template>
   <div id="admin-panel">
     <app-header></app-header>
-    <b-modal id="modal-scoped">
+    <b-modal id="modal-audio">
       <template v-slot:modal-header>
-        <h5>Usuń produkt</h5>
+        <h5>Dodaj audio</h5>
       </template>
-      <div class="d-block">Czy na pewno chcesz usunąć podcast?</div>
+      <form>
+        <div class="form-group">
+          <label for="audioFile">Plik audio</label>
+          <input
+            type="file"
+            class="form-control-file"
+            id="audioFile"
+            accept=".mp3"
+            v-on:change="onFileChange"
+          />
+          <small id="fileHelp" class="form-text text-muted">Plik powinien być w formacie mp3</small>
+        </div>
+      </form>
       <template v-slot:modal-footer="{ ok, cancel }">
         <b-button size="sm" @click="cancel()">Wyjdź</b-button>
-        <b-button size="sm" @click="deleteItem">Usuń</b-button>
+        <b-button size="sm" @click="addAudio">Dodaj</b-button>
+      </template>
+    </b-modal>
+    <b-modal id="modal-scoped">
+      <template v-slot:modal-header>
+        <h5>Dodaj podcast</h5>
+      </template>
+      <form>
+        <div class="form-group">
+          <label for="title">Tytuł</label>
+          <input
+            type="text"
+            class="form-control"
+            id="title"
+            placeholder="Wprowadź tytuł podcastu"
+            v-model="podcastToAdd.title"
+          />
+        </div>
+        <div class="form-group">
+          <label for="description">Opis</label>
+          <input
+            type="text"
+            class="form-control"
+            id="description"
+            placeholder="Wprowadź opis podcastu"
+            v-model="podcastToAdd.description"
+          />
+        </div>
+        <div class="form-group">
+          <label for="price">Cena</label>
+          <input
+            type="number"
+            class="form-control"
+            id="price"
+            placeholder="Wprowadź cenę podcastu"
+            v-model="podcastToAdd.price"
+          />
+        </div>
+        <div class="form-group">
+          <label for="length">Długość</label>
+          <input
+            type="number"
+            class="form-control"
+            id="length"
+            placeholder="Wprowadź długość podcastu"
+            v-model="podcastToAdd.length"
+          />
+        </div>
+        <div class="form-group">
+          <label for="imageUrl">Url do zdjęcia</label>
+          <input
+            type="text"
+            class="form-control"
+            id="imageUrl"
+            placeholder="Wprowadź url do zdjęcia okładkowego"
+            v-model="podcastToAdd.imageUrl"
+          />
+        </div>
+        <label>Kategoria</label>
+        <select class="form-control form-control-sm" v-model="podcastToAdd.category">
+          <option disabled value>Wybierz kategorię</option>
+          <option value="mindset">Rozwój osobisty</option>
+          <option value="money">Pieniądze</option>
+          <option value="ecology">Ekologia</option>
+          <option value="politics">Polityka</option>
+        </select>
+
+        <label>Nazwa pliku</label>
+        <select class="form-control form-control-sm" v-model="podcastToAdd.fileName">
+          <option disabled value>Wybierz nazwę pliku</option>
+          <option v-for="fileName in filesNames" :key="fileName">{{fileName}}</option>
+        </select>
+      </form>
+      <template v-slot:modal-footer="{ ok, cancel }">
+        <b-button size="sm" @click="cancel()">Wyjdź</b-button>
+        <b-button size="sm" @click="addPodcast">Dodaj</b-button>
       </template>
     </b-modal>
     <div class="card">
       <div class="card-body">
-        <a class="add">Dodaj produkt</a>
+        <a class="add" @click="showModal">Dodaj podcast</a>
+        <a class="add" @click="showAudioModal">Dodaj audio</a>
         <table class="table table-striped">
           <thead>
             <tr>
@@ -32,7 +120,7 @@
               <td style="text-align:center;" v-if="podcast.price!=null">{{podcast.price}} zł</td>
               <td style="text-align:center;" v-else>-</td>
               <td style="text-align:center;">
-                <a class="edit">Edytuj</a>
+                <a class="edit" @click="showModal">Edytuj</a>
               </td>
             </tr>
           </tbody>
@@ -53,16 +141,26 @@ export default {
       src: "",
       isInCart: false,
       modalShow: false,
-      podcastToDelete: ""
+      filesNames: [],
+      podcastToAdd: {
+        title: "",
+        description: "",
+        category: "",
+        price: null,
+        length: null,
+        fileName: "",
+        file: null
+      }
     };
   },
   beforeMount() {
     this.getPodcasts();
+    this.getFileNames();
   },
   methods: {
-    getPodcasts() {
+    getFileNames() {
       this.$http
-        .get("http://localhost:8081/podcasts", {
+        .get("http://localhost:8081/podcasts/names", {
           headers: {
             Authorization: this.$cookie.get("jwt")
           }
@@ -82,12 +180,68 @@ export default {
           for (let key in data) {
             resultArray.push(data[key]);
           }
+          this.filesNames = resultArray;
+          console.log(this.filesNames);
+        });
+    },
+    addAudio() {
+      let formData = new FormData();
+      formData.append("audioFile", this.file);
+      this.$http
+        .post("http://localhost:8081/podcasts/audio", formData, {
+          headers: {
+            Authorization: this.$cookie.get("jwt"),
+            "Content-Type": "multipart/form-data"
+          }
+        })
+        .then(
+          response => {
+            this.getFileNames();
+            return response.json();
+          },
+          error => {
+            document.getElementById("error-span").innerHTML =
+              "Aby zobaczyć listę podcastów należy się zalogować jako admin.";
+            console.log(error);
+          }
+        );
+    },
+    getPodcasts() {
+      this.$http
+        .get("http://localhost:8081/podcasts", {
+          headers: {
+            Authorization: this.$cookie.get("jwt")
+          }
+        })
+        .then(
+          response => {
+            this.getFileNames();
+            return response.json();
+          },
+          error => {
+            document.getElementById("error-span").innerHTML =
+              "Aby zobaczyć listę podcastów należy się zalogować jako admin.";
+            console.log(error);
+          }
+        )
+        .then(data => {
+          const resultArray = [];
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
           this.podcasts = resultArray;
           console.log(this.podcasts);
         });
     },
+    onFileChange($event) {
+      this.file = $event.target.files[0];
+    },
     showModal() {
       this.$root.$emit("bv::show::modal", "modal-scoped", "#btnShow");
+    },
+
+    showAudioModal() {
+      this.$root.$emit("bv::show::modal", "modal-audio", "#btnShow");
     },
     hideModal() {
       this.$root.$emit("bv::hide::modal", "modal-scoped", "#btnShow");
